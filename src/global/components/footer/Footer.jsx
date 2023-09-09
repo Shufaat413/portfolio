19637e9ceee732a5f545fa52ed57597c7e07fbd7
Shufaat413 <alishufaat@gmail.com>
2023-09-09 04:28:48 -0700

    update complete page

--- src/global/components/footer/Footer.jsx
+++ src/global/components/footer/Footer.jsx
@@ -3,9 +3,18 @@ import "./Footer.scss";
 
 const Footer = () => {
     return (
+        <>
         <div className="footerContainer">
-            footer
+            <div className="left">
+                <span>Shufaat Ali</span><a href="mailto:alishufaat@gmail.com">alishufaat@gmail.com</a>
+                <p>We design it and develope Front-end and back-end it</p>
+            </div>
+            <div className="right">
+                <p>Media</p>
+            </div>
         </div>
+        <p id="fot-p">&copy; Copyright 2023, Made by Shufaat Ali </p>
+        </>
     )
 }
 
